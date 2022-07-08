7d3289a44e886c826df3ee13f97f3d9241d0c8c3
Muhammed Eroğlu <llofmejb@gmail.com>
2022-07-08 21:17:34 +0300

    3

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,28 @@
-import React from 'react'
+
+import { useState } from 'react'
+import { Nav, Logo, Hamburger, Menu, MenuLink } from './style'
 
 const Navbar = () => {
+
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <Nav>
+      <Logo to="/home" >
+        <i>{"<Recipe>"}</i>
+        <span>app</span>
+      </Logo>
+      <Hamburger onClick={() => setOpen(!isOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </Hamburger>
+      <Menu isOpen={isOpen}>
+        <MenuLink to="/about">About</MenuLink>
+        <MenuLink to="/github">Github</MenuLink>
+        <MenuLink to="/">Logout</MenuLink>
+      </Menu>
+    </Nav>
   )
 }
 
