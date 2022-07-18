46d13cd6c28fe8612dbd3fa4272b3a0fe4474e15
Muhammed Eroğlu <llofmejb@gmail.com>
2022-07-18 13:07:33 +0300

    3

--- src/pages/home/RecipeCardComp.js
+++ src/pages/home/RecipeCardComp.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import { RecipeCard, RecipeHeader } from './style'
+import { Navigate, useNavigate } from 'react-router-dom';
+import { RecipeCard, RecipeHeader, RecipeImage, Button } from './style'
 
 const RecipeCardComp = ({recipe, index}) => {
+
+  const navigate = useNavigate();
+  // Linkte veri gönderme sıkıntı olduğu için navigasyonu bu şekilde yaptık...
+  const moreClick = () => {
+    navigate("/details", {state:{recipe}})
+  }
+
   return (
     <RecipeCard >
       <RecipeHeader>
-        
+        {recipe.label}
       </RecipeHeader>
+      <RecipeImage src={recipe.image}/>
+      <Button onClick={moreClick}>View More</Button>
     </RecipeCard>
   )
 }
