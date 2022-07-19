99daced6f8eea2a87821e4de150170a1c4ddd512
Muhammed Eroğlu <llofmejb@gmail.com>
2022-07-19 19:10:29 +0300

    6

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
         {/* <GiHamburgerMenu/> */}
       </Hamburger>
       <Menu isOpen={isOpen}>
-        <MenuLink to="/about" onClick={()=> setOpen(!isOpen)}>bout</MenuLink>
+        <MenuLink to="/about" onClick={()=> setOpen(!isOpen)}>About</MenuLink>
         <MenuLink to="/" onClick={()=> {sessionStorage.clear(); setOpen(!isOpen)}} >Logout</MenuLink>
       </Menu>
     </Nav>
